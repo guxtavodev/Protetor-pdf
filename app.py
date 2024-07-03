e3416016f167022a60dc83c067652d31dbbb7eb1
Guxtavo <88405978+guxtavodev@users.noreply.github.com>
2024-07-02 22:59:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def index():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
